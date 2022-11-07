4088d7d8206c02d0df39f3e650a4cf835b9653a8
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-07 16:16:42 +0100

    responsive for details page added

--- src/pages/details/DetailsStyles.jsx
+++ src/pages/details/DetailsStyles.jsx
@@ -11,6 +11,7 @@ export const DetailContainer = styled.div`
   /* height: fit-content; */
   padding: 5px;
   text-align: center;
+  width: 100%;
 `;
 
 export const DetailPart = styled.div`
@@ -21,7 +22,6 @@ export const DetailPart = styled.div`
   flex-wrap: wrap;
   align-items: center;
   margin: 1rem;
-  width: 300px;
 
   border-radius: 5px;
   color: wheat;
@@ -29,8 +29,9 @@ export const DetailPart = styled.div`
   /* height: calc(100vh - 300px); */
   @media (max-width: 1250px) {
     justify-content: center;
-
-    border: 1px solid red;
+  }
+  @media (max-width: 726px) {
+    font-size: 1rem;
   }
 `;
 
@@ -44,10 +45,14 @@ export const ImgContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
+  margin: 0 auto 20px auto;
+  width: 80%;
   img {
-    height: 300px;
+    width: 30%;
     border-radius: 10px;
+  }
+  @media (max-width: 726px) {
+    font-size: 0.5rem;
   }
 `;
 
@@ -58,6 +63,7 @@ export const HeaderContainer = styled.div`
   justify-content: space-evenly;
   margin: 25px;
   font-size: 3rem;
+  width: 90%;
   h1 {
     font-size: 3rem;
   }
@@ -68,15 +74,18 @@ export const IngredContainer = styled.div`
   width: 400px;
   font-size: 1.4rem;
   margin: 2rem;
+  width: 30%;
 
   border-radius: 10px;
   color: wheat;
+  @media (max-width: 726px) {
+    font-size: 1rem;
+  }
 `;
 
 export const OtherPart = styled.div`
   /* üstteki bilgiler */
   background-color: lightgray;
-
   width: 400px;
   text-align: right;
   font-size: 1.8rem;
